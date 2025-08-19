c0b8056cc3a539544cbd27fe71ee6dbc1a4c85ba
b13rg <13488329+B13rg@users.noreply.github.com>
2025-08-18 18:25:47 -0700

    add server code

---
+++ pkg/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+func respIndex(w http.ResponseWriter, req *http.Request) {
+	var tmplFile = "index.tmpl.html"
+	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
+	if err != nil {
+		fmt.Fprintf(w, "Error parsing template\n")
+		return
+	}
+	err = tmpl.Execute(w, req)
+	if err != nil {
+		fmt.Fprintf(w, "Error executing template\n")
+		return
+	}
+	// for name, headers := range req.Header {
+	// 	for _, h := range headers {
+	// 		fmt.Fprintf(w, "%v: %v\n", name, h)
+	// 	}
+	// }
+}
+
+func respIP(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "%v", req.RemoteAddr)
+}
+
+// Start serving on specified port.
+// If port is <= 0, defaults to 8080.
+func Serve(port int) {
+	if port <= 0 {
+		port = 8080
+	}
+	http.HandleFunc("/", respIndex)
+	http.HandleFunc("/ip", respIP)
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+}
